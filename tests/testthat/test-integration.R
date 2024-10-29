92244f797536f705e8239a5ea62d7abfaba7b49a
Josiah Parry <josiah.parry@gmail.com>
2024-10-29 12:15:02 -0700

    closes #8

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -17,3 +17,19 @@ test_that("dates and ints are returned", {
   res <- arc_select(cities_fl, n_max = 10)
   expect_true(is.numeric(res$POP_CLASS))
 })
+
+test_that("empty queries return null", {
+  skip_on_cran()
+  skip_if_not_installed("arcgislayers")
+  library(arcgislayers)
+  layer <- arc_open(
+    "https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/USA_Counties_Generalized_Boundaries/FeatureServer/0"
+  )
+
+  res <- arc_select(
+    layer,
+    where = "STATE_NAME LIKE 'Marylnd'"
+  )
+
+  expect_null(res)
+})
